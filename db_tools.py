a69080fba9288b80c250443e50c695ac8b2a3a49
Justin F <bookboy210@jforseth.tech>
2019-06-08 15:15:01 -0600

    First commit

---
+++ db_tools.py
@@ -0,0 +1,162 @@
+import sqlite3
+import random
+
+###########
+#Acccounts#
+############
+# These are actual logins, with passwords, not just emails!
+
+
+def get_accounts():
+    conn = sqlite3.connect('database.db')
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM accounts")
+    account_data = cur.fetchall()
+    accounts = {}
+    for i in account_data:
+        username = i[0]
+        password = i[1]
+        has_access_to = i[2].split(',')
+
+        tempdict = {}
+        tempdict.update({'password': password})
+        tempdict.update({'has_access_to': has_access_to})
+        accounts[username] = tempdict
+    return(accounts)
+
+###########
+#Messenger#
+###########
+
+
+def add_message(result):
+    conn = sqlite3.connect('database.db')
+    cur = conn.cursor()
+    with conn:
+        cur.execute("INSERT INTO messages VALUES(:result)", {'result': result})
+
+
+def read_messages():
+    conn = sqlite3.connect('database.db')
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM messages")
+    return cur.fetchall()
+
+
+def clear_messages():
+    conn = sqlite3.connect('database.db')
+    cur = conn.cursor()
+    with conn:
+        cur.execute("DELETE FROM messages")
+        cur.execute("VACUUM")
+########
+#Prayer#
+########
+
+
+def get_verification_code():
+    file = open('text/validcodes.txt', 'r')
+    validcodes = file.readline()
+    file.close()
+    randomNum = random.randint(0, len(validcodes)-5)
+    code = validcodes[randomNum:randomNum+5]
+    return code
+
+
+def get_verification_email_template():
+    file = open('text/verification_email_template.html')
+    verification_email = file.read()
+    file.close()
+    return verification_email
+
+
+def add_to_mailing_list(address, parish):
+    conn = sqlite3.connect("database.db")
+    cur = conn.cursor()
+    with conn:
+        cur.execute("""INSERT INTO users VALUES(:email,:parish)""",
+                    {'email': address, 'parish': parish})
+
+
+def check_verification_code(code):
+    file = open('text/validcodes.txt', 'r')
+    validcodes = file.readline()
+    return code in validcodes
+
+
+def read_prayer_request_template(name, prequest, parish):
+    file = open("text/prayer_request_email_template.html")
+    template = file.read()
+    file.close()
+
+    subject = '{} has sent a prayer request to {}'
+    subject = subject.format(name, parish)
+    message = template.format(name, parish, prequest)
+
+    return(message, subject)
+
+
+def get_emails_from_parish(parish):
+    conn = sqlite3.connect("database.db")
+    cur = conn.cursor()
+    with conn:
+        cur.execute("""SELECT * FROM users WHERE parish=:parish""",
+                    {'parish': parish})
+    emails = cur.fetchall()
+    emails = [i[0] for i in emails]
+    return emails
+
+######
+#TODO#
+######
+
+
+def get_todos():
+    file = open('text/todo.csv', 'r')
+    todos = file.readlines()
+    file.close()
+    return todos
+
+
+def add_todo(name):
+    file = open('text/todo.csv', 'a')
+    file.write('{}\n'.format(name))
+    file.close()
+
+def delete_todo(taskid):
+    file = open("text/todo.csv", 'r')
+    items = file.readlines()
+    file.close()
+   
+    #This line is magic. No idea what's going on. 
+    items.pop(len(items)-taskid)
+
+    file = open("text/todo.csv", 'w')
+    for i in items:
+        file.write(i)
+    file.close()
+
+def reorder_todo(item_to_reorder,position_to_move):
+        file = open("text/todo.csv", 'r')
+        items = file.readlines()
+        file.close()
+        
+        item_to_reorder = items[len(items)-item_to_reorder]
+        position_to_move = len(items)-position_to_move
+        items.remove(item_to_reorder)
+        items.insert(position_to_move, item_to_reorder)
+        file = open("text/todo.csv", 'w')
+        # Now that the item has been reordered, rewrite the file.
+        for i in items:
+            file.write(i)
+        file.close()
+#######
+#Admin#
+#######
+def get_all_from_table(table):
+        conn = sqlite3.connect('database.db')
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM {}".format(table))
+        return cur.fetchall()
+if __name__ == "__main__":
+    print("This is the database management code for jforseth.tech. It is not intended for any other use case.")
---
+++ static/simple_mail.py
@@ -0,0 +1,32 @@
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+def send_email(email_recipient, email_subject, email_message, sender, password):
+    s = smtplib.SMTP(host='smtp.gmail.com', port='587')
+    s.starttls()
+    s.login(sender, password)
+    # Create the message
+    msg = MIMEMultipart()
+    msg['From'] = sender
+    msg['To'] = email_recipient
+    msg['Subject'] = email_subject
+    # The template for the messenger.
+    # The form elements are invisible, the user just has to click submit.
+    msg.attach(MIMEText(email_message, 'html'))
+
+    s.sendmail(msg['From'], msg['To'], msg.as_string())
+    del msg
+    s.quit()
+def simple_mail_help():
+    print("This is a module for easily sending emails. To use it, import it into your program and call send_email().")
+    print("Arguments:")
+    print("Email recipient: Who you're sending the email too.")    
+    print("Email subject: What the subject line should be.")
+    print("Email message: What the message should be. Sent as HTML but works with plaintext.")
+    print("Sender: The account to send from. CURRENTLY ONLY WORKS WITH GMAIL.")
+    print("Password: The passwords of the account to send from.")
+    print("If using a new account, make sure less secure access is turned ON.")
+if __name__ == "__main__":
+    simple_mail_help()
+    
