29acc99de2b25a69a28bd88c49098bf985849e25
Justin <jforseth210@gmail.com>
2019-10-06 16:38:31 -0600

    Figlet!

--- webtool.py
+++ webtool.py
@@ -22,10 +22,13 @@ app.config['SECRET_KEY'] = SECRET_KEY
 # Account management
 SimpleLogin(app, login_checker=check_login)
 
-#Contains:
-#/httpforwarding
-app.register_blueprint(http_forwarding)
 
+
+# __        __   _
+# \ \      / /__| | ___ ___  _ __ ___   ___
+#  \ \ /\ / / _ \ |/ __/ _ \| '_ ` _ \ / _ \
+#   \ V  V /  __/ | (_| (_) | | | | | |  __/
+#    \_/\_/ \___|_|\___\___/|_| |_| |_|\___|
 #Contains:
 #/
 #/FlaskApp
@@ -34,6 +37,13 @@ app.register_blueprint(http_forwarding)
 #/menu
 app.register_blueprint(welcome)
 
+
+
+# __     ___     _
+# \ \   / (_) __| | ___  ___  ___
+#  \ \ / /| |/ _` |/ _ \/ _ \/ __|
+#   \ V / | | (_| |  __/ (_) \__ \
+#    \_/  |_|\__,_|\___|\___/|___/
 #Contains:
 #/videos
 #/videos/newupload
@@ -43,14 +53,27 @@ app.register_blueprint(welcome)
 #/videos/move
 app.register_blueprint(videos)
     
-#Contains:
+
+#  __  __
+# |  \/  | ___  ___ ___  ___ _ __   __ _  ___ _ __
+# | |\/| |/ _ \/ __/ __|/ _ \ '_ \ / _` |/ _ \ '__|
+# | |  | |  __/\__ \__ \  __/ | | | (_| |  __/ |
+# |_|  |_|\___||___/___/\___|_| |_|\__, |\___|_|
+# Contains:                        |___/
 #/messenger
 #/messenger/result
 #/message/stream
 #/messenger/clear
 app.register_blueprint(messenger)
 
-#Contains
+
+
+#  ____
+# |  _ \ _ __ __ _ _   _  ___ _ __
+# | |_) | '__/ _` | | | |/ _ \ '__|
+# |  __/| | | (_| | |_| |  __/ |
+# |_|   |_|  \__,_|\__, |\___|_|
+# Contains         |___/
 #/prayer
 #/FlaskApp/prayer
 #/prayer/newemail
@@ -58,6 +81,13 @@ app.register_blueprint(messenger)
 #/prayer/prayerrequest
 app.register_blueprint(prayer)
 
+
+
+#  _____         _
+# |_   _|__   __| | ___
+#   | |/ _ \ / _` |/ _ \
+#   | | (_) | (_| | (_) |
+#   |_|\___/ \__,_|\___/
 #Contains:
 #/todolocal
 #/todo/submitted
@@ -65,6 +95,13 @@ app.register_blueprint(prayer)
 #/todo/reorder
 app.register_blueprint(todo)
 
+
+
+#   ___        _      _       _
+#  / _ \ _   _(_) ___| | ____| |_ __ __ ___      __
+# | | | | | | | |/ __| |/ / _` | '__/ _` \ \ /\ / /
+# | |_| | |_| | | (__|   < (_| | | | (_| |\ V  V /
+#  \__\_\\__,_|_|\___|_|\_\__,_|_|  \__,_| \_/\_/
 #Contains:
 #/quickdraw
 #/quickdraw/shot
@@ -72,7 +109,14 @@ app.register_blueprint(todo)
 #/quickdraw/bigscreen/begin
 app.register_blueprint(quickdraw_game)    
 
-#Contains:
+
+
+#  ____        _ _       _           _       _
+# | __ ) _   _| | |     | |_   _  __| | __ _(_)_ __   __ _
+# |  _ \| | | | | |  _  | | | | |/ _` |/ _` | | '_ \ / _` |
+# | |_) | |_| | | | | |_| | |_| | (_| | (_| | | | | | (_| |
+# |____/ \__,_|_|_|  \___/ \__,_|\__,_|\__, |_|_| |_|\__, |
+# Contains:                             |___/         |___/
 #/bulljudging
 #/bulljudging1
 #/bulljudging2
@@ -81,11 +125,24 @@ app.register_blueprint(quickdraw_game)
 #/bulljudgingdone
 app.register_blueprint(bull_judging)
 
-#Contains:
-#/DBbrowser
-app.register_blueprint(admin)
 
-#Contains:
+#     _       _           _
+#    / \   __| |_ __ ___ (_)_ __
+#   / _ \ / _` | '_ ` _ \| | '_ \
+#  / ___ \ (_| | | | | | | | | | |
+# /_/   \_\__,_|_| |_| |_|_|_| |_|
+# #Contains:
+#/DBbrowser
+#app.register_blueprint(admin)
+
+
+
+#  ____                      _   ____            _
+# | __ )  __ _ _ __ _ __ ___| | |  _ \ __ _  ___(_)_ __   __ _
+# |  _ \ / _` | '__| '__/ _ \ | | |_) / _` |/ __| | '_ \ / _` |
+# | |_) | (_| | |  | | |  __/ | |  _ < (_| | (__| | | | | (_| |
+# |____/ \__,_|_|  |_|  \___|_| |_| \_\__,_|\___|_|_| |_|\__, |
+# Contains:                                               |___/
 #/barrelracing/app_lab
 #/barrelracing/counter
 #/barrelracing/counter/currentnumber
@@ -93,23 +150,35 @@ app.register_blueprint(admin)
 #/barrelracing/stream
 app.register_blueprint(barrel_racing)
 
-#Contains:
+
+#  _____ _ _        ____  _                _
+# |  ___(_) | ___  / ___|| |__   __ _ _ __(_)_ __   __ _
+# | |_  | | |/ _ \ \___ \| '_ \ / _` | '__| | '_ \ / _` |
+# |  _| | | |  __/  ___) | | | | (_| | |  | | | | | (_| |
+# |_|   |_|_|\___| |____/|_| |_|\__,_|_|  |_|_| |_|\__, |
+# Contains:                                         |___/
 #/filesharing
 #/filesharing/<filename>
 #/filesharing/filelist
 app.register_blueprint(file_sharing)
-#Error handlers
+
+
+#  _____                       _   _                 _ _
+# | ____|_ __ _ __ ___  _ __  | | | | __ _ _ __   __| | | ___ _ __ ___
+# |  _| | '__| '__/ _ \| '__| | |_| |/ _` | '_ \ / _` | |/ _ \ '__/ __|
+# | |___| |  | | | (_) | |    |  _  | (_| | | | | (_| | |  __/ |  \__ \
+# |_____|_|  |_|  \___/|_|    |_| |_|\__,_|_| |_|\__,_|_|\___|_|  |___/
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template('404.html'), 404
+    return render_template('404.html')
 
 @app.errorhandler(403)
 def forbidden(e):
-    return render_template('403.html'), 403
+    return render_template('403.html')
 
 @app.errorhandler(500)
 def server_error(e):
-    return render_template('500.html'), 500
+    return render_template('500.html')
 
 
 if __name__ == "__main__":
