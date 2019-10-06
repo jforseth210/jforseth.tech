a1b93bd77d9e81b050b5e9ecc9cd377d028e681e
Justin <jforseth210@gmail.com>
2019-10-05 21:44:05 -0600

    Fixed a bug in http forwarding

--- scripts/http_forwarding.py
+++ scripts/http_forwarding.py
@@ -9,26 +9,26 @@ def experiment():
     
     requested_url=request.args.get('url')
     
-    #Did the user request an image?
-    ispng = requested_url[:-4]==".png"
-    isjpg = requested_url[:-4]==".jpg"
-    isjpeg = requested_url[:-5]!=".jpeg"
-    
-    isimage = ispng or isjpg or isjpeg
+
 
     #If the user hasn't entered a url yet, return this message. 
     if requested_url == None:
         page="Enter your desired page"
-    elif isimage:
+        return "<form><input name='url' /><input type='submit'></form>"+str(page)
+    #Did the user request an image?
+    ispng = requested_url[:-4]==".png"
+    isjpg = requested_url[:-4]==".jpg"
+    isjpeg = requested_url[:-5]==".jpeg"
+    
+    isimage = ispng or isjpg or isjpeg
+    if isimage:
         # If the user has requested an image, send it raw, not as text.
-        
-        if ispng or isjpeg or isjpg:
-            return requests.get(requested_url).response_raw	
-        else:
-        	page=requests.get(requested_url).content
+        return str(requests.get(requested_url).raw)	
+    else:
+        page=requests.get(requested_url).content
         requested_url_utf=requested_url.encode('utf-8')
         for attribute in ATTRIBUTES:
-            #page=page.replace(attribute.encode('utf-8')+b"=",attribute.encode('utf-8')+b"=http://jforseth.tech/experiment?url="+requested_url_utf)
+        #page=page.replace(attribute.encode('utf-8')+b"=",attribute.encode('utf-8')+b"=http://jforseth.tech/experiment?url="+requested_url_utf)
             page=page.replace(attribute.encode('utf-8')+b"='/",attribute.encode('utf-8')+b'=http://jforseth.tech/experiment?url='+requested_url_utf+b'/')
             page=page.replace(attribute.encode('utf-8')+b'=\"/',attribute.encode('utf-8')+b'=http://jforseth.tech/experiment?url='+requested_url_utf+b'/')
             page=page.replace(attribute.encode('utf-8')+b'=/',attribute.encode('utf-8')+b'=http://jforseth.tech/experiment?url='+requested_url_utf+b'/')
@@ -41,4 +41,5 @@ def experiment():
         #page=page.replace(b'src=/',b'src=http://jforseth.tech/experiment?url='+requested_url.encode('utf-8')+b'/')
         #page=page.replace(b'href=/',b'href=http://jforseth.tech/experiment?url='+requested_url.encode('utf-8')+b'/')
         #page=page.replace(b'content=/',b'content=http://jforseth.tech/experiment?url='+requested_url.encode('utf-8')+b'/')
-    return "<form><input name='url' /><input type='submit'></form>"+str(page)
+
+        return "<form><input name='url' /><input type='submit'></form>"+str(page)
