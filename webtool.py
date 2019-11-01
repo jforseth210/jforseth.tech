4cc32e18e0ba5c21e483da3206d0e3538a39d1be
Justin <jforseth210@gmail.com>
2019-10-31 18:09:34 -0600

    Dummy commit

--- webtool.py
+++ webtool.py
@@ -49,7 +49,7 @@ def deploy():
 
 @app.route("/dummy")
 def dummy():
-    return "It updated, PEBKAC!"
+    return "It updated, dummy!"
 # __        __   _
 # \ \      / /__| | ___ ___  _ __ ___   ___
 #  \ \ /\ / / _ \ |/ __/ _ \| '_ ` _ \ / _ \
