8327c4217717bd4880f0700a729f531214b29e2d
Justin <jforseth210@gmail.com>
2019-11-09 17:56:15 -0700

    More accurate information

--- scripts/lucky_shoe.py
+++ scripts/lucky_shoe.py
@@ -5,7 +5,7 @@ lucky_shoe = Blueprint('lucky_shoe', __name__)  # Main page
 
 @lucky_shoe.route('/luckyshoe')
 def lucky_shoe_home():
-    return render_template('lucky_shoe/luckyshoe.html', PRICE="PRICE PLACEHOLDER")
+    return render_template('lucky_shoe/luckyshoe.html')
 
 @lucky_shoe.route('/luckyshoe/order', methods=["POST"])
 def lucky_shoe_order():
