8e51ffda1ab188077b91c02fbb6650c2b753fdee
Justin <jforseth210@gmail.com>
2019-11-09 17:10:49 -0700

    Lucky shoe changes

--- scripts/lucky_shoe.py
+++ scripts/lucky_shoe.py
@@ -1,6 +1,19 @@
 from flask import *
+from simple_mail import send_email
+from SensitiveData import *
 lucky_shoe = Blueprint('lucky_shoe', __name__)  # Main page
 
 @lucky_shoe.route('/luckyshoe')
 def lucky_shoe_home():
     return render_template('lucky_shoe/luckyshoe.html', PRICE="PRICE PLACEHOLDER")
+
+@lucky_shoe.route('/luckyshoe/order', methods=["POST"])
+def lucky_shoe_order():
+    rq=request.form.to_dict()
+    paramlist=[]
+    for key, val in rq.items():
+        paramlist.append("{}: {}".format(key, val))
+    paramlist="<br />".join(paramlist)
+    send_email("luckyshoe@jforseth.tech","New Horseshoe Order", paramlist, PROJECT_EMAIL,PROJECT_PASSWORD) 
+    flash("Order submitted sucessfully.", category="success")
+    return redirect("/luckyshoe")
