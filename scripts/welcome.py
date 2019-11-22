19e5b9d2ae2ddfa1343e0334016cd5476358834c
Justin <jforseth210@gmail.com>
2019-11-21 21:02:34 -0700

    Custom account page

--- scripts/welcome.py
+++ scripts/welcome.py
@@ -32,8 +32,10 @@ def menu():
 def signup():
     return render_template('welcome/signup.html')
 
-@welcome.route('/account')
-def account():
-    account=get_account(get_username())
-    account.pop("hashed_password")
-    return render_template('welcome/account.html',account=account)
+@welcome.route('/account/<account>')
+def account(account):
+    if account == get_username():
+        account=get_account(get_username())
+        account.pop("hashed_password")
+        return render_template('welcome/account.html',account=account)
+    return render_template("errors/403.html"), 403
