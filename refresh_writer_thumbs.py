d80fcdef16f19f49c3183b43654fce5433c276bb
Justin <jforseth210@gmail.com>
2019-11-28 19:49:01 -0700

    Update refresh_writer_thumbs to work on windows and linux

--- refresh_writer_thumbs.py
+++ refresh_writer_thumbs.py
@@ -1,15 +1,18 @@
 import imgkit
 import os
-WRITER_PATH="/var/www/html/text/writerdocs"
-THUMB_PATH="/var/www/html/static/writer_thumbs"
+WRITER_PATH="text/writerdocs"
+THUMB_PATH="static/writer_thumbs"
 options={
     'height':'300',
     'width':'500',
-    'encoding':'utf-8',
-    "xvfb": ""
+    'encoding':'utf-8'
 }
-
+print(os.name)
+if os.name == 'nt':
+    config=imgkit.config(wkhtmltoimage=b"C:/Program Files/wkhtmltopdf/bin/wkhtmltoimage.exe")
+else:
+    config=imgkit.config()
 files = os.listdir(WRITER_PATH)
 for i in files:
-    print(WRITER_PATH+"/"+i, THUMB_PATH+"/"+i+"_thumb.png", options)
-    imgkit.from_file(WRITER_PATH+"/"+i, THUMB_PATH+"/"+i+"_thumb.png", options=options)
+    print(WRITER_PATH+"/"+i, THUMB_PATH+"/"+i+"_thumb.png", options,config)
+    imgkit.from_file(WRITER_PATH+"/"+i, THUMB_PATH+"/"+i+"_thumb.png", options=options,config=config)
