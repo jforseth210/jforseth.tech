b34d3f9baf1004da47a5c0932a0151ec7350019a
jforseth210 <jforseth210@GMAIL.COM>
2019-11-28 19:11:16 -0700

    Fixed html to img

--- refresh_writer_thumbs.py
+++ refresh_writer_thumbs.py
@@ -3,12 +3,13 @@ import os
 WRITER_PATH="/var/www/html/text/writerdocs"
 THUMB_PATH="/var/www/html/static/writer_thumbs"
 options={
-    'crop-h':'300',
-    'crop-w':'500',
-    'encoding':'utf-8',   
-    'zoom':'200'
+    'height':'300',
+    'width':'500',
+    'encoding':'utf-8',
+    "xvfb": ""
 }
 
 files = os.listdir(WRITER_PATH)
 for i in files:
+    print(WRITER_PATH+"/"+i, THUMB_PATH+"/"+i+"_thumb.png", options)
     imgkit.from_file(WRITER_PATH+"/"+i, THUMB_PATH+"/"+i+"_thumb.png", options=options)
