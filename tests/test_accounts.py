41972686af6b2d5c7b9e11f9264ef666a634556d
Justin Forseth <jforseth210@gmail.com>
2020-06-15 10:49:55 -0600

    Basic login tests. (Still need edge cases, signups, account_page.)

--- tests/test_accounts.py
+++ tests/test_accounts.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 from webtool import app
 from SensitiveData import PROJECT_PASSWORD
 
-class FlaskTestCase(unittest.TestCase):
+class AccountsTestCase(unittest.TestCase):
     def test_login_page(self):
         tester = app.test_client(self)
         response = tester.get('/login/', content_type='html/text')
@@ -12,10 +12,37 @@ class FlaskTestCase(unittest.TestCase):
         self.assertTrue(b"Password" in response.data)
         self.assertTrue(b"Forgot Password" in response.data)
 
-    def test_login(self):
+    def test_correct_login(self):
         with app.test_client() as tester:
             response = tester.get('/login/')
             soup = BeautifulSoup(response.data, 'html.parser')
             token = soup.find(id='csrf_token')['value']
             response = tester.post('/login', data=dict(csrf_token=token, username='testing', password=PROJECT_PASSWORD, next='/'), follow_redirects=True)
             self.assertIn(b"Login Successful", response.data)
+            self.assertEqual(response.status_code, 200)
+
+    def test_correct_login_no_csrf(self):
+        with app.test_client() as tester:
+            response = tester.get('/login/')
+            response = tester.post('/login', data=dict(username='testing', password=PROJECT_PASSWORD, next='/'), follow_redirects=True)
+            self.assertIn(b"token is missing", response.data)
+            self.assertEqual(response.status_code, 200)
+
+    def test_incorrect_username(self):
+        with app.test_client() as tester:
+            response = tester.get('/login/')
+            soup = BeautifulSoup(response.data, 'html.parser')
+            token = soup.find(id='csrf_token')['value']
+            response = tester.post('/login', data=dict(csrf_token=token, username='thisuserdoesnotexist', password=PROJECT_PASSWORD, next='/'), follow_redirects=True)
+            self.assertIn(b"Account not found", response.data)
+            self.assertEqual(response.status_code, 401)
+
+
+    def test_incorrect_password(self):
+        with app.test_client() as tester:
+            response = tester.get('/login/')
+            soup = BeautifulSoup(response.data, 'html.parser')
+            token = soup.find(id='csrf_token')['value']
+            response = tester.post('/login', data=dict(csrf_token=token, username='testing', password='this is the wrong password', next='/'), follow_redirects=True)
+            self.assertIn(b"Incorrect password", response.data)
+            self.assertEqual(response.status_code, 401)
