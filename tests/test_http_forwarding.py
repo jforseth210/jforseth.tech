6623594799261a9e4a317d4f87b90131cfafd0b9
Justin Forseth <jforseth210@gmail.com>
2020-06-15 13:16:26 -0600

    HTTP Forwarding unit test boilerplate

---
+++ tests/test_http_forwarding.py
@@ -0,0 +1,11 @@
+import unittest
+from webtool import app
+
+class HTTPForwardingTestCase(unittest.TestCase):
+    def setUp(self):
+        pass
+    def tearDown(self):
+        pass
+
+    def test_HTTPForwarding(self):
+        pass
