e882cde635e5ecd53bdf7c1d99cfd51bb52f4d59
Justin Forseth <jforseth210@gmail.com>
2020-06-15 21:23:27 -0600

    Lucky shoe unit tests

---
+++ tests/test_lucky_shoe.py
@@ -0,0 +1,39 @@
+import unittest
+from webtool import app
+from twilio.rest import Client
+from SensitiveData import *
+
+
+class LuckyShoeTestCase(unittest.TestCase):
+    def setUp(self):
+        app.config["TESTING"] = True
+
+    def tearDown(self):
+        pass
+
+    def test_lucky_shoe_page(self):
+        tester = app.test_client(self)
+        response = tester.get('/luckyshoe')
+        self.assertEqual(200, response.status_code)
+        self.assertIn(b'Lucky Shoe Welding', response.data)
+
+    def test_lucky_shoe_order(self):
+        data = dict(testing='True',
+                    testing_well='False', code_working='True', code_good='False')
+        tester = app.test_client(self)
+        response = tester.post('/luckyshoe/order', data=data)
+        self.assertIs(200, response.status_code)
+        for key, value in data.items():
+            self.assertIn(key, str(response.data))
+            self.assertIn(value, str(response.data))
+
+    def test_twilio_working(self):
+        account_sid = TWILIO_SID
+        auth_token = TWILIO_TOKEN
+        client = Client(account_sid, auth_token)
+        # If we make it this far without errors, it works.
+        self.assertTrue(True)
+
+
+if __name__ == "__main__":
+    unittest.main()
