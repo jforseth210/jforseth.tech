4432c38c732c9d1e08e63fdd4a73cc772c908a94
Justin Forseth <jforseth210@gmail.com>
2020-06-15 19:08:49 -0600

    Reformating

--- tests/test_accounts.py
+++ tests/test_accounts.py
@@ -9,52 +9,61 @@ from SensitiveData import PROJECT_PASSWORD
 #TODO: Password changes
 #TODO: Prayer additions and unsubcriptions (maybe in test_prayer).
 #TODO: Account deletion
+def login(app):
+    response = app.get('/login/')
+    soup = BeautifulSoup(response.data, 'html.parser')
+    token = soup.find(id='csrf_token')['value']
+    response = app.post('/login', data=dict(csrf_token=token, username='testing', password=PROJECT_PASSWORD, next='/'), follow_redirects=True)
+    return response
+
 class AccountsTestCase(unittest.TestCase):
+    def setUp(self):
+        app.testing = True
+        self.app = app.test_client()
+
     def test_login_page(self):
-        tester = app.test_client(self)
-        response = tester.get('/login/', content_type='html/text')
+        response = self.app.get('/login/', content_type='html/text')
         self.assertEqual(response.status_code, 200)
         self.assertTrue(b"Username" in response.data)
         self.assertTrue(b"Password" in response.data)
         self.assertTrue(b"Forgot Password" in response.data)
 
     def test_correct_login(self):
-        with app.test_client() as tester:
-            response = tester.get('/login/')
-            soup = BeautifulSoup(response.data, 'html.parser')
-            token = soup.find(id='csrf_token')['value']
-            response = tester.post('/login', data=dict(csrf_token=token, username='testing', password=PROJECT_PASSWORD, next='/'), follow_redirects=True)
+        with app:
+            response = login(app)
             self.assertIn(b"Login Successful", response.data)
             self.assertEqual(response.status_code, 200)
 
     def test_correct_login_no_csrf(self):
-        with app.test_client() as tester:
-            response = tester.get('/login/')
-            response = tester.post('/login', data=dict(username='testing', password=PROJECT_PASSWORD, next='/'), follow_redirects=True)
+        with app:
+            response = app.get('/login/')
+            response = app.post('/login', data=dict(username='testing', password=PROJECT_PASSWORD, next='/'), follow_redirects=True)
             self.assertIn(b"token is missing", response.data)
             self.assertEqual(response.status_code, 200)
 
     def test_incorrect_username(self):
-        with app.test_client() as tester:
-            response = tester.get('/login/')
+        with app:
+            response = app.get('/login/')
             soup = BeautifulSoup(response.data, 'html.parser')
             token = soup.find(id='csrf_token')['value']
-            response = tester.post('/login', data=dict(csrf_token=token, username='thisuserdoesnotexist', password=PROJECT_PASSWORD, next='/'), follow_redirects=True)
+            response = app.post('/login', data=dict(csrf_token=token, username='thisuserdoesnotexist', password=PROJECT_PASSWORD, next='/'), follow_redirects=True)
             self.assertIn(b"Account not found", response.data)
             self.assertEqual(response.status_code, 401)
 
     def test_incorrect_password(self):
-        with app.test_client() as tester:
-            response = tester.get('/login/')
+        with app:
+            response = app.get('/login/')
             soup = BeautifulSoup(response.data, 'html.parser')
             token = soup.find(id='csrf_token')['value']
-            response = tester.post('/login', data=dict(csrf_token=token, username='testing', password='this is the wrong password', next='/'), follow_redirects=True)
+            response = app.post('/login', data=dict(csrf_token=token, username='testing', password='this is the wrong password', next='/'), follow_redirects=True)
             self.assertIn(b"Incorrect password", response.data)
             self.assertEqual(response.status_code, 401)
 
     def test_account_no_login(self):
-        tester = app.test_client(self)
-        response = tester.get('/account/testing',
+        response = app.get('/account/testing',
                               content_type='html/text', follow_redirects=False)
         self.assertEqual(response.status_code, 403)
         self.assertTrue(b"Oops" in response.data)
+
+if __name__ == '__main__':
+    unittest.main()
