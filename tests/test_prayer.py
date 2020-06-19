eb80844b69da49b18b922e897d63759cce4ed193
Justin Forseth <jforseth210@gmail.com>
2020-06-18 21:01:14 -0600

    Cleanup prayer test.

--- tests/test_prayer.py
+++ tests/test_prayer.py
@@ -1,6 +1,7 @@
 import unittest
 import json
 from webtool import app
+import html
 
 
 class PrayerTestCase(unittest.TestCase):
@@ -17,17 +18,24 @@ class PrayerTestCase(unittest.TestCase):
         self.assertTrue(b"Request Prayer" in response.data)
 
     def test_prayer_request(self):
+        name = "Testing"
+        parish = "TestParish"
+        prequest = "This should only go to the test user. If it doesn't, something has broken. Please email me at support@jforseth.tech"
         tester = app.test_client(self)
         response = tester.post(
             "/prayer/prayerrequest",
             data=dict(
-                name="Testing",
-                parish="Testing",
-                prequest="This should only go to the test user. If it doesn't, something has broken. Please email me at support@jforseth.tech",
+                name=name, 
+                parish=parish,
+                prequest=prequest
             ),
         )
         responsedict = json.loads(response.data.decode("utf-8"))
-        self.assertIn("testing@jforseth.tech", responsedict["emails"])
+        self.assertEqual(type(responsedict['emails']), type([]))
+        self.assertIn(parish,responsedict['subject_template'])
+        self.assertIn(name,responsedict['subject_template'])
+        self.assertIn(prequest,html.unescape(responsedict['message_template']))
+
 
 
 if __name__ == "__main__":
