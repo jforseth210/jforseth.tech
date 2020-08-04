95aea9f2f22bda1f3b386522432b2214def34d67
Justin <jforseth210@gmail.com>
2020-08-03 19:02:17 -0600

    Prayer subscription tests

--- tests/test_prayer.py
+++ tests/test_prayer.py
@@ -1,6 +1,8 @@
 import unittest
 import json
 from webtool import app
+from SensitiveData import TESTING_GROUP_SIGNUP_CODE
+from test_accounts import signup, login
 import html
 
 
@@ -32,6 +34,25 @@ class PrayerTestCase(unittest.TestCase):
         self.assertIn(name, responsedict["subject_template"])
         self.assertIn(prequest, html.unescape(responsedict["message_template"]))
 
+    def test_prayer_subscription(self):
+        with app.test_client() as tester:
+            signup(tester, parish="")
+            login(tester)
+            response = tester.post(
+                "/prayer/addgroup",
+                data=dict(group=TESTING_GROUP_SIGNUP_CODE),
+                follow_redirects=True,
+            )
+        with app.test_client() as tester:
+            login(tester)
+            response = tester.get("/account/testing")
+            self.assertTrue(b"Testing" in response.data)
+            self.assertEqual(200, response.status_code)
 
+    def test_prayer_unsubscription(self):
+        with app.test_client() as tester:
+            signup(tester)
+            login(tester)
+            tester.get('/prayer/unsubscribe_logged_in', ) 
 if __name__ == "__main__":
     unittest.main()
